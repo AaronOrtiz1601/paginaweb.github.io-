4bd56873b58fa194e86510daeb4d79ea3e759fc1
AaronOrtiz1601 <148748614+AaronOrtiz1601@users.noreply.github.com>
2024-05-22 23:23:27 +0200

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -1,7 +1,7 @@
 let contador = 0;
 let contplay = 0;
 let contluna = 0;
-let header = document.querySelector('header')
+let header = document.querySelector('header');
 let texto = document.querySelector('h1');
 let imagen = document.getElementById('imagen');
 let Izquierda = document.querySelector('.miBotonIz'); 
@@ -11,62 +11,25 @@ let pause = document.getElementById('play');
 let sol = document.getElementById('luna');
 let body = document.querySelector('body');
 
+// Array de URLs de imágenes
+let imagenes = [
+    "https://i.ytimg.com/vi/R2dPqUtyLRI/maxresdefault.jpg",
+    "https://e1.pxfuel.com/desktop-wallpaper/20/158/desktop-wallpaper-pinterest-cosmere.jpg",
+    "https://i.redd.it/x2c4vkrqa2361.jpg",
+    "https://cosmere.es/wp-content/uploads/2023/11/DRAGONSTEEL23-BROTHERWISE-GAMES-The-Stormlight-Archive-TTRPG_Kaladin-en-las-Llanuras-Quebradas.png",
+    "https://i.pinimg.com/736x/75/d2/fa/75d2fa512458a1c7529cd411232a5ec7.jpg",
+    "https://i.ytimg.com/vi/NuaD9uC9VAY/maxresdefault.jpg",
+    "https://wallpapercave.com/wp/wp7574435.jpg"
+];
+
 function cambioImagen() {
-    contador++;
-    if (contador == 0) {
-        imagen.src = "https://i.ytimg.com/vi/R2dPqUtyLRI/maxresdefault.jpg";
-    }
-    if (contador == 1) {
-        imagen.src = "https://e1.pxfuel.com/desktop-wallpaper/20/158/desktop-wallpaper-pinterest-cosmere.jpg";
-    }
-    if (contador == 2) {
-        imagen.src = "https://i.redd.it/x2c4vkrqa2361.jpg";
-    }
-    if (contador == 3) {
-        imagen.src = "https://cosmere.es/wp-content/uploads/2023/11/DRAGONSTEEL23-BROTHERWISE-GAMES-The-Stormlight-Archive-TTRPG_Kaladin-en-las-Llanuras-Quebradas.png";
-    }
-    if (contador == 4) {
-        imagen.src = "https://i.pinimg.com/736x/75/d2/fa/75d2fa512458a1c7529cd411232a5ec7.jpg";
-    }
-    if (contador == 5) {
-        imagen.src = "https://i.ytimg.com/vi/NuaD9uC9VAY/maxresdefault.jpg";
-    }
-    if (contador > 6) {
-        contador = -1; 
-    }
-    if (contador == 6) {
-        imagen.src = "https://wallpapercave.com/wp/wp7574435.jpg";
-        contador = contador - 7;
-    }
+    contador = (contador + 1) % imagenes.length;
+    imagen.src = imagenes[contador];
 }
+
 function Inverso() {
-    contador--;
-    if (contador == 0) {
-        imagen.src = "https://i.ytimg.com/vi/R2dPqUtyLRI/maxresdefault.jpg";
-        contador = contador + 7;
-    }
-    if (contador == 1) {
-        imagen.src = "https://e1.pxfuel.com/desktop-wallpaper/20/158/desktop-wallpaper-pinterest-cosmere.jpg";
-    }
-    if (contador == 2) {
-        imagen.src = "https://i.redd.it/x2c4vkrqa2361.jpg";
-    }
-    if (contador == 3) {
-        imagen.src = "https://cosmere.es/wp-content/uploads/2023/11/DRAGONSTEEL23-BROTHERWISE-GAMES-The-Stormlight-Archive-TTRPG_Kaladin-en-las-Llanuras-Quebradas.png";
-    }
-    if (contador == 4) {
-        imagen.src = "https://i.pinimg.com/736x/75/d2/fa/75d2fa512458a1c7529cd411232a5ec7.jpg";
-    }
-    if (contador == 5) {
-        imagen.src = "https://i.ytimg.com/vi/NuaD9uC9VAY/maxresdefault.jpg";
-    }
-    
-    if (contador < 0) {
-        contador = 7; 
-    }
-    if (contador == 6) {
-        imagen.src = "https://wallpapercave.com/wp/wp7574435.jpg";
-    }
+    contador = (contador - 1 + imagenes.length) % imagenes.length;
+    imagen.src = imagenes[contador];
 }
 
 Derecha.addEventListener("click", cambioImagen); 
@@ -84,20 +47,21 @@ function play() {
     pause.src = "https://cdn-icons-png.flaticon.com/128/4980/4980096.png";
     contplay++;
   }
-  if (contplay == 2)
-  {
+  if (contplay == 2) {
     contplay = 0;
   }
 }
+
 sol.addEventListener("click",luna);
+
 function luna() {
     if (contluna == 0) {
         body.style.background = "#121728";
         header.style.background = "#252850";
-        texto.style.color = " #c6b1c9";
+        texto.style.color = "#c6b1c9";
         sol.src = "https://cdn-icons-png.flaticon.com/128/2917/2917242.png";
         lunaActiva = !lunaActiva;
-        localStorage.setItem('lunaActiva',lunaActiva);
+        localStorage.setItem('lunaActiva', lunaActiva);
         contluna++;
     } else {
         body.style.background = "";
@@ -105,18 +69,36 @@ function luna() {
         texto.style.color = "";
         sol.src = "https://cdn-icons-png.flaticon.com/128/3594/3594375.png";
         lunaActiva = !lunaActiva;
-        localStorage.setItem('lunaActiva',lunaActiva);
+        localStorage.setItem('lunaActiva', lunaActiva);
         contluna++;
     }
-    if (contluna == 2)
-    {
+    if (contluna == 2) {
         contluna = 0;
     }
 }
+
 let lunaActiva = false;
-window.onload = ()=> {
+window.onload = () => {
     let saveMode = localStorage.getItem('lunaActiva');
-    if (saveMode === 'true'){
+    if (saveMode === 'true') {
         luna();
     }
+};
+
+// Función AJAX para obtener nuevas imágenes
+function cargarImagenes() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.ejemplo.com/obtener-imagenes', true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const nuevasImagenes = JSON.parse(xhr.responseText);
+            imagenes = nuevasImagenes;
+            contador = 0;
+            imagen.src = imagenes[contador];
+        }
+    };
+    xhr.send();
 }
+
+// Event listener para cargar nuevas imágenes con AJAX
+document.getElementById('cargarNuevasImagenes').addEventListener('click', cargarImagenes);
